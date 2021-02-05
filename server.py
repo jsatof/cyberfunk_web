88e69ae584a33e99c7920f94f4d4a5682b6d7174
jsferrarelli <jsferr4@gmail.com>
2021-02-05 17:15:12 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+server = Flask(__name__)
+
+@server.route("/", methods = ["GET"])
+def index():
+    return "hello"
+
+if __name__ == "__main__":
+    server.run(host = "0.0.0.0")
