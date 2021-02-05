c210150ace0fd8000a98dcf372faf0a4b435f964
jsferrarelli <jsferr4@gmail.com>
2021-02-05 17:59:44 -0500

    Move app.py to app/main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-server = Flask(__name__)
+app = Flask(__name__)
 
-@server.route("/", methods = ["GET"])
+@app.route("/", methods = ["GET"])
 def index():
     return "hello"
 
 if __name__ == "__main__":
-    server.run()
+    app.run()
