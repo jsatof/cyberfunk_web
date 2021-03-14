1b8004bf341f42d68132ef18e59dc4927bfac31f
jsferrarelli <jsferr4@gmail.com>
2021-03-13 21:20:27 -0500

    Add route for post testing

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET"])
 def index():
     return render_template("index.html")
+
+@app.route("/post", methods = ["POST"])
+def post():
+    if request.json:
+        return request.json
+
+    return "bad request"
 
 if __name__ == "__main__":
     app.run()
