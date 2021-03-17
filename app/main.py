1c2e868407d2179cd594a2bb8e8db2cdf04d6312
jsferrarelli <jsferr4@gmail.com>
2021-03-17 09:23:49 -0400

    Move helper functions into main.py

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ from flask_restful import Resource, Api, reqparse
 import psycopg2
 import os
 
-from database_functions import *
-
 app = Flask(__name__)
 api = Api(app)
 
@@ -31,6 +29,64 @@ class Login(Resource):
 
 
 app.add_resource(Login, "/api/login")
+
+
+# Database Functions
+
+def create_tables(connection): # assuming connection is working
+
+    # append commands when want to create more tables on init
+    commands = (
+        """
+        CREATE TABLE users (
+            uid SERIAL PRIMARY KEY,
+            username VARCHAR(16) NOT NULL,
+            password VARCHAR(16) NOT NULL
+        )
+        """)
+
+    
+
+    try:
+        params = config()
+
+        cursor = connection.cursor()
+
+        for command in commands:
+            cursor.execute(command)
+
+        cursor.close()
+        connection.commit()
+    
+    except(Exception. psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if connection is not None:
+            connection.close()
+
+if __name__ == "__main__":
+    create_tables()
+import psycopg2
+
+def user_lookup(connection):
+    try:
+        cursor = connection.cursor()
+        query = "SELECT uid, username, password FROM users ORDER BY uid"
+
+        cursor.execute(query)
+
+        matches = []        
+
+        row = cursor.fetchone()
+
+        cursor.close()
+
+
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
 
 if __name__ == "__main__":
     app.run()
