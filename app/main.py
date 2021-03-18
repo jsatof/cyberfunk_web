db3a5f2a323c8189425eb5f8d775d9bb51bf1d39
jsferrarelli <jsferr4@gmail.com>
2021-03-18 12:11:29 -0400

    Clean

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ import os
 app = Flask(__name__)
 api = Api(app)
 
-DATABASE_URL = os.environ["DATABASE_URL"]
-connection = psycopg2.connect(DATABASE_URL, sslmode = "require")
+#DATABASE_URL = os.environ["DATABASE_URL"]
+#connection = psycopg2.connect(DATABASE_URL, sslmode = "require")
 
 parser = reqparse.RequestParser()
 
@@ -28,64 +28,6 @@ class Login(Resource):
         return jsonify(userame = uname, password = pword)
 
 api.add_resource(Login, "/api/login")
-
-
-# Database Functions
-
-def create_tables(connection): # assuming connection is working
-
-    # append commands when want to create more tables on init
-    commands = (
-        """
-        CREATE TABLE users (
-            uid SERIAL PRIMARY KEY,
-            username VARCHAR(16) NOT NULL,
-            password VARCHAR(16) NOT NULL
-        )
-        """)
-
-    
-
-    try:
-        params = config()
-
-        cursor = connection.cursor()
-
-        for command in commands:
-            cursor.execute(command)
-
-        cursor.close()
-        connection.commit()
-    
-    except(Exception. psycopg2.DatabaseError) as error:
-        print(error)
-
-    finally:
-        if connection is not None:
-            connection.close()
-
-if __name__ == "__main__":
-    create_tables()
-import psycopg2
-
-def user_lookup(connection):
-    try:
-        cursor = connection.cursor()
-        query = "SELECT uid, username, password FROM users ORDER BY uid"
-
-        cursor.execute(query)
-
-        matches = []        
-
-        row = cursor.fetchone()
-
-        cursor.close()
-
-
-
-    except(Exception, psycopg2.DatabaseError) as error:
-        print(error)
-
 
 if __name__ == "__main__":
     app.run()
