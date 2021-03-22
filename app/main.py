eacfe56c6e641a240d085e8a13028945b7bff1fe
jsferrarelli <jsferr4@gmail.com>
2021-03-22 08:07:44 -0400

    correct routes and create database table

--- app/main.py
+++ app/main.py
@@ -1,33 +1,38 @@
 from flask import Flask, render_template, request
-from flask_restful import Resource, Api, reqparse
+from flask_sqlalchemy import SQLAlchemy
+
 import psycopg2
 import os
 
 app = Flask(__name__)
-api = Api(app)
 
-#DATABASE_URL = os.environ["DATABASE_URL"]
-#connection = psycopg2.connect(DATABASE_URL, sslmode = "require")
+database = SQLAlchemy(app)
 
-parser = reqparse.RequestParser()
+class Users(database.Model):
+    __tablename__ = "Users"
+    userid = database.Column(database.Integer, primary_key = True)
+    username = database.Column(database.String(32), unique = True)
+    password = database.Column(database.String(16), unique = False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+connection = psycopg2.connect(DATABASE_URL, sslmode = "require")
 
 @app.route("/", methods = ["GET"])
 def index():
     return render_template("index.html")
 
-# API Routes are below
-class Login(Resource):
-    def post(self):
-        json_data = request.get_json(force = True)
-        
-        # STUB: check for valid args
+@app.route("/login", methods = ["POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
-        uname = json_data["username"]
-        pword = json_data["password"]
+        if username == "" or password == "":
+            return render_template("login.html", message = "Please Enter Required Fields")
 
-        return jsonify(userame = uname, password = pword)
-
-api.add_resource(Login, "/api/login")
 
 if __name__ == "__main__":
     app.run()
